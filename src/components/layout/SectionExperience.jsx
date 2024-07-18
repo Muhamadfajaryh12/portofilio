0de9e8e32a1851fdb25198822cf1bbd36f554999
Muhamadfajaryh12 <2010631170090@student.unsika.ac.id>
2024-07-18 14:27:47 +0700

    adj

--- src/components/layout/SectionExperience.jsx
+++ src/components/layout/SectionExperience.jsx
@@ -115,79 +115,85 @@ const SectionExperience = () => {
       >
         - EXPERIENCE -
       </h2>
-      <div style={{ maxWidth: "1200px", overflow: "auto" }}>
-        <table className="table bg-transparent border-secondary table-bordered text-center">
-          <thead>
-            <tr>
-              <th className="bg-transparent text-white">Timeline</th>
-              <th className="bg-transparent text-white">Jan</th>
-              <th className="bg-transparent text-white">Feb</th>
-              <th className="bg-transparent text-white">Mar</th>
-              <th className="bg-transparent text-white">Apr</th>
-              <th className="bg-transparent text-white">May</th>
-              <th className="bg-transparent text-white">Jun</th>
-              <th className="bg-transparent text-white">Jul</th>
-              <th className="bg-transparent text-white">Aug</th>
-              <th className="bg-transparent text-white">Sep</th>
-              <th className="bg-transparent text-white">Oct</th>
-              <th className="bg-transparent text-white">Nov</th>
-              <th className="bg-transparent text-white">Dec</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Object.keys(groupedData).map((year) => (
-              <tr key={year}>
-                <td className="bg-transparent text-white">{year}</td>
-                {[
-                  "Jan",
-                  "Feb",
-                  "Mar",
-                  "Apr",
-                  "May",
-                  "Jun",
-                  "Jul",
-                  "Aug",
-                  "Sep",
-                  "Oct",
-                  "Nov",
-                  "Dec",
-                ].map((month) => (
-                  <td
-                    key={`${year}-${month}`}
-                    className="bg-transparent text-white"
-                  >
-                    {groupedData[year].find((item) => item.month === month) ? (
-                      <button className=" btn btn-sm  ">
-                        <span
-                          className={`badge ${
-                            groupedData[year].find(
-                              (item) => item.month === month
-                            ).activity == "Internship"
-                              ? "bg-success"
-                              : groupedData[year].find(
-                                  (item) => item.month === month
-                                ).activity == "Study Independent"
-                              ? "bg-primary"
-                              : "bg-danger"
-                          }`}
-                          style={{ width: "10px !important;" }}
-                        >
-                          {
-                            groupedData[year].find(
-                              (item) => item.month === month
-                            ).activity
-                          }
-                        </span>
-                      </button>
-                    ) : (
-                      ""
-                    )}
-                  </td>
-                ))}
+      <div className="d-flex justify-content-center">
+        <div
+          style={{ maxWidth: "1400px", minWidth: "1000px", overflow: "auto" }}
+        >
+          <table className="table bg-transparent border-secondary table-bordered text-center">
+            <thead>
+              <tr>
+                <th className="bg-transparent text-white">Timeline</th>
+                <th className="bg-transparent text-white">Jan</th>
+                <th className="bg-transparent text-white">Feb</th>
+                <th className="bg-transparent text-white">Mar</th>
+                <th className="bg-transparent text-white">Apr</th>
+                <th className="bg-transparent text-white">May</th>
+                <th className="bg-transparent text-white">Jun</th>
+                <th className="bg-transparent text-white">Jul</th>
+                <th className="bg-transparent text-white">Aug</th>
+                <th className="bg-transparent text-white">Sep</th>
+                <th className="bg-transparent text-white">Oct</th>
+                <th className="bg-transparent text-white">Nov</th>
+                <th className="bg-transparent text-white">Dec</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {Object.keys(groupedData).map((year) => (
+                <tr key={year}>
+                  <td className="bg-transparent text-white">{year}</td>
+                  {[
+                    "Jan",
+                    "Feb",
+                    "Mar",
+                    "Apr",
+                    "May",
+                    "Jun",
+                    "Jul",
+                    "Aug",
+                    "Sep",
+                    "Oct",
+                    "Nov",
+                    "Dec",
+                  ].map((month) => (
+                    <td
+                      key={`${year}-${month}`}
+                      className="bg-transparent text-white"
+                    >
+                      {groupedData[year].find(
+                        (item) => item.month === month
+                      ) ? (
+                        <button className=" btn btn-sm  ">
+                          <span
+                            className={`badge ${
+                              groupedData[year].find(
+                                (item) => item.month === month
+                              ).activity == "Internship"
+                                ? "bg-success"
+                                : groupedData[year].find(
+                                    (item) => item.month === month
+                                  ).activity == "Study Independent"
+                                ? "bg-primary"
+                                : "bg-danger"
+                            }`}
+                            style={{ width: "10px !important;" }}
+                          >
+                            {
+                              groupedData[year].find(
+                                (item) => item.month === month
+                              ).activity
+                            }
+                          </span>
+                        </button>
+                      ) : (
+                        ""
+                      )}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
--- src/components/layout/SectionProject.jsx
+++ src/components/layout/SectionProject.jsx
@@ -1,6 +1,17 @@
 import React from "react";
-
+import { useState } from "react";
+import { Card, Col, Row, Nav } from "react-bootstrap";
+import data from "../../utils/project.json";
 const SectionProject = () => {
+  const [category, setCategory] = useState("");
+  const typeClick = (props) => () => {
+    setCategory(props);
+  };
+
+  const filteredData = category
+    ? data.filter((item) => item.category == category)
+    : data;
+
   return (
     <div className="bg-dark p-5" id="project">
       <h2
@@ -9,6 +20,29 @@ const SectionProject = () => {
       >
         - PROJECT -
       </h2>
+      <Nav fill variant="tabs" defaultActiveKey="" className="my-4">
+        <Nav.Item>
+          <Nav.Link eventKey="link-1" onClick={typeClick("real")}>
+            Real Project
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2" onClick={typeClick("dummy")}>
+            Dummy Project
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <Row xs={1} md={3} className="g-4">
+        {filteredData.map((item, index) => (
+          <Col key={index}>
+            <Card className="h-100 text-center text-white">
+              <Card.Body>
+                <Card.Title>{item.name}</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 };
